f942160ccea251f7e55add6bb07a1fede79ef0b3
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-06-26 02:11:54 -0500

    removed obfuscation

--- features.js
+++ features.js
@@ -1,1 +1,102 @@
-function _0x2f1e(_0x4370c2,_0x36020d){const _0x3f39d1=_0x3f39();return _0x2f1e=function(_0x2f1e76,_0x3d0efe){_0x2f1e76=_0x2f1e76-0x1b9;let _0x48de31=_0x3f39d1[_0x2f1e76];return _0x48de31;},_0x2f1e(_0x4370c2,_0x36020d);}function _0x3f39(){const _0x34d017=['throwablesInView','spawn','#5597ff','teamColors','strokeStyle','radius','18LrsaGq','#ddd763','set','fillStyle','791QaKHTa','704KbmOIm','475288qEKQPd','socket','get','teamColorsSolid','3323795khHvhE','split','3673830uDgyiv','delete','addEventListener','decode','54450jvWocm','hookWS','446440VBffJs','update','#ff2d00','557472fsAVsq','#00ff11','type','36405IZqaXE','#ed5555','teamCode'];_0x3f39=function(){return _0x34d017;};return _0x3f39();}(function(_0x5721ed,_0x4f55a5){const _0x1feb09=_0x2f1e,_0xe5c1e0=_0x5721ed();while(!![]){try{const _0x4ac5de=parseInt(_0x1feb09(0x1c2))/0x1+-parseInt(_0x1feb09(0x1c5))/0x2+parseInt(_0x1feb09(0x1d1))/0x3*(-parseInt(_0x1feb09(0x1d7))/0x4)+-parseInt(_0x1feb09(0x1ba))/0x5+-parseInt(_0x1feb09(0x1c0))/0x6*(-parseInt(_0x1feb09(0x1d5))/0x7)+-parseInt(_0x1feb09(0x1d6))/0x8*(-parseInt(_0x1feb09(0x1c8))/0x9)+parseInt(_0x1feb09(0x1bc))/0xa;if(_0x4ac5de===_0x4f55a5)break;else _0xe5c1e0['push'](_0xe5c1e0['shift']());}catch(_0x595f68){_0xe5c1e0['push'](_0xe5c1e0['shift']());}}}(_0x3f39,0x838cf),((()=>{const _0x519e51=_0x2f1e;class _0x14632e{static [_0x519e51(0x1ce)]=[_0x519e51(0x1c9),'#3cd157',_0x519e51(0x1cd)];static [_0x519e51(0x1b9)]=[_0x519e51(0x1c4),_0x519e51(0x1c6),'#002cff'];constructor(_0x5d9dfe){const _0x12b764=_0x519e51;this[_0x12b764(0x1d8)]=_0x5d9dfe,this[_0x12b764(0x1cb)]=new Map(),this[_0x12b764(0x1d8)][_0x12b764(0x1be)]('message',_0x310e08=>{try{_0xa0788f(_0x310e08);}catch(_0x107c27){}});}[_0x519e51(0x1cc)](_0x123f6c){const _0x222b14=_0x519e51;this[_0x222b14(0x1ca)]=_0x123f6c;}}class _0x11cfe6{constructor(_0x5ee040,_0x1e671b,_0x4d2127,_0x58f305,_0x2c7248,_0x54191d){const _0x47e338=_0x519e51;this['id']=_0x5ee040,this[_0x47e338(0x1c7)]=_0x1e671b,this['x']=_0x4d2127,this['y']=_0x58f305,this[_0x47e338(0x1d0)]=_0x2c7248,this[_0x47e338(0x1ca)]=_0x54191d;}[_0x519e51(0x1c3)](_0x59062c,_0x2ec772,_0x2001be){const _0x56d014=_0x519e51;this['x']=_0x59062c,this['y']=_0x2ec772,this[_0x56d014(0x1d0)]=_0x2001be;}}function _0xa0788f(_0x43100d){const _0x5c8f8f=_0x519e51;let _0x56f171=new TextDecoder()[_0x5c8f8f(0x1bf)](_0x43100d['data']),_0x21f8fc=_0x56f171[_0x5c8f8f(0x1bb)]('|');for(let _0x3f0520 of _0x21f8fc){let _0x1a9446=_0x3f0520[_0x5c8f8f(0x1bb)](','),_0x36f753=_0x1a9446[0x0];switch(_0x36f753){case'.':{break;}case'a':{let _0x445158=+_0x1a9446[0x2];_0x38303c['spawn'](_0x445158);break;}case'm':{let _0xf4025f=+_0x1a9446[0x1],_0x1d1aa4=+_0x1a9446[0x2],_0x4a9b14=+_0x1a9446[0x3],_0x5d6ff1=+_0x1a9446[0x4],_0x382aeb=+_0x1a9446[0x8],_0x2f1f0c=new _0x11cfe6(_0xf4025f,_0x1d1aa4,_0x4a9b14,_0x5d6ff1,0x0,_0x382aeb);_0x38303c[_0x5c8f8f(0x1cb)][_0x5c8f8f(0x1d3)](_0xf4025f,_0x2f1f0c);break;}case'n':{let _0x46f3fd=+_0x1a9446[0x1],_0x23dcea=+_0x1a9446[0x2],_0x2c6118=+_0x1a9446[0x3],_0x72e9af=+_0x1a9446[0x8];_0x38303c[_0x5c8f8f(0x1cb)][_0x5c8f8f(0x1d9)](_0x46f3fd)[_0x5c8f8f(0x1c3)](_0x23dcea,_0x2c6118,_0x72e9af);break;}case'o':{let _0x4bbd31=+_0x1a9446[0x1];_0x38303c['throwablesInView'][_0x5c8f8f(0x1bd)](_0x4bbd31);break;}}}}let _0x38303c;window[_0x519e51(0x1c1)]=function(_0x69d541){_0x38303c=new _0x14632e(_0x69d541);};let _0x157cb3=setInterval(()=>{ASM_CONSTS&&(ASM_CONSTS[0x210a8]=function(_0x20a8fd,_0x42f133,_0x4289ca,_0x2092f4,_0x49ae1a,_0x1a8e41){const _0x2af0a6=_0x2f1e;if(contexts[_0x20a8fd][_0x2af0a6(0x1d4)]==_0x2af0a6(0x1d2))for(let [_0x49ac3f,_0x449759]of _0x38303c[_0x2af0a6(0x1cb)]){_0x2092f4==_0x449759['radius']&&(contexts[_0x20a8fd]['fillStyle']=_0x14632e[_0x2af0a6(0x1ce)][_0x449759[_0x2af0a6(0x1ca)]],contexts[_0x20a8fd][_0x2af0a6(0x1cf)]=_0x14632e[_0x2af0a6(0x1b9)][_0x449759[_0x2af0a6(0x1ca)]]);}contexts[_0x20a8fd]['arc'](_0x42f133,_0x4289ca,_0x2092f4,_0x49ae1a,_0x1a8e41);},clearInterval(_0x157cb3));},0x10);})()));
+(() => {
+    //Classes
+    class Game {
+        static teamColors = ["#ed5555", "#3cd157", "#5597ff"];
+        static teamColorsSolid = ["#ff2d00", "#00ff11", "#002cff"];
+        constructor(socket) {
+            this.socket = socket;
+            this.throwablesInView = new Map();
+            this.socket.addEventListener("message", packet => {
+                try {
+                    onMessage(packet);
+                } catch (e) {}
+            });
+        }
+        spawn(teamCode) {
+            this.teamCode = teamCode;
+        }
+    }
+
+    class Throwable {
+        constructor(id, type, x, y, radius, teamCode) {
+            this.id = id;
+            this.type = type;
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.teamCode = teamCode;
+        }
+        update(x, y, radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+    }
+
+    //Handlers
+    function onMessage(packet) {
+        let packetDecoded = new TextDecoder().decode(packet.data);
+        let statePackets = packetDecoded.split("|");
+        for (let statePacket of statePackets) {
+            let parts = statePacket.split(",");
+            let opcode = parts[0];
+            switch (opcode) {
+                case ".": {
+                    break;
+                }
+                case "a": {
+                    let teamCode = +parts[2];
+                    game.spawn(teamCode);
+                    break;
+                }
+                case "m": {
+                    let grenadeID = +parts[1];
+                    let type = +parts[2];
+                    let x = +parts[3];
+                    let y = +parts[4];
+                    let teamCode = +parts[8];
+                    let grenade = new Throwable(grenadeID, type, x, y, 0, teamCode);
+                    game.throwablesInView.set(grenadeID, grenade);
+                    break;
+                }
+                case "n": {
+                    let grenadeID = +parts[1];
+                    let x = +parts[2];
+                    let y = +parts[3];
+                    let radius = +parts[8];
+                    game.throwablesInView.get(grenadeID).update(x, y, radius);
+                    break;
+                }
+                case "o": {
+                    let grenadeID = +parts[1];
+                    game.throwablesInView.delete(grenadeID);
+                    break;
+                }
+            }
+        }
+    }
+
+    //Init
+    let game;
+
+    window.hookWS = function(socket) {
+        game = new Game(socket);
+    }
+
+    let asmConstsOverride = setInterval(() => {
+        if (ASM_CONSTS) {
+            ASM_CONSTS[135336] = function($0, $1, $2, $3, $4, $5) {
+                if (contexts[$0].fillStyle == "#ddd763")  {
+                    for (let [_id, throwable] of game.throwablesInView) {
+                        if ($3 == throwable.radius) {
+                            contexts[$0].fillStyle = Game.teamColors[throwable.teamCode];
+                            contexts[$0].strokeStyle = Game.teamColorsSolid[throwable.teamCode];
+                        }
+                    }
+                }
+                contexts[$0].arc($1 , $2 , $3 , $4, $5);
+            }
+            clearInterval(asmConstsOverride);
+        }
+    }, 16);
+})();
