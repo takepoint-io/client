2f6fb87fefe70e1bcab6c4d7f4cf68ed431a0a84
nitrogem35 <nitrogem9@gmail.com>
2025-06-05 14:03:20 -0500

    Revert "Update features.js"
    This reverts commit ac7659d24620d54161a38566d76b2df2f4650e25.

--- features.js
+++ features.js
@@ -18,14 +18,13 @@
             this.slashCommands = [];
             this.chatbox = document.getElementById("chatbox");
             this.wasTyping = false;
-            //ugh
-            // this.chatboxLoop = setInterval(() => {
-            //     if (!chatboxOpen) return;
-            //     let dots = "." + ".".repeat(Math.floor(Date.now() / 300) % 3);
-            //     let packet = new Packet().setOpcode("c").setParams(dots);
-            //     this.socket.send(packet.encode());
-            //     this.wasTyping = true;
-            // }, 300);
+            this.chatboxLoop = setInterval(() => {
+                if (!chatboxOpen) return;
+                let dots = "." + ".".repeat(Math.floor(Date.now() / 300) % 3);
+                let packet = new Packet().setOpcode("c").setParams(dots);
+                this.socket.send(packet.encode());
+                this.wasTyping = true;
+            }, 300);
             this.socket.addEventListener("message", packet => {
                 try {
                     onMessage(packet);
